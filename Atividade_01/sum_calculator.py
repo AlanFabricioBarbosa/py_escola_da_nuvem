a33f21d53d9af671c8affcf8344496b67a73c0bb
Alan Fabrício <alanfabriciobarbosa1@gmail.com>
2025-10-06 23:09:30 -0300

    Adicionando a primeira lista de atividades

---
+++ Atividade_01/sum_calculator.py
@@ -0,0 +1,6 @@
+number1 = 12
+number2 = 14
+
+sum_result = number1 + number2
+
+print(f"A soma de {number1} + {number2} = {sum_result}")
---
+++ Atividade_01/volume_calculator.py
@@ -0,0 +1,11 @@
+length = 12 
+width = 14 
+height = 20 
+
+volume = length * width * height
+
+print(f"Dimensões da caixa:")
+print(f"Comprimento: {length} cm")
+print(f"Largura: {width} cm")
+print(f"Altura: {height} cm")
+print(f"Volume: {volume} cm³")
